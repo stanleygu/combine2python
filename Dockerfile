0661002b64647b2cc40393a7e9c14e3e384d2b1e
Stanley Gu <stanleygu@gmail.com>
2014-09-15 19:56:51 +0000

    Updated to fully create ipython notebook from archive

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,10 @@ FROM        stanleygu/sysbio-workspace:1.0.9
 MAINTAINER  Stanley Gu "stanleygu@gmail.com"
 
 # zerorpc server
+RUN         su user -c "source /usr/local/bin/virtualenvwrapper.sh; workon localpy; pip install click==3.3"
 USER        user
-RUN         /home/user/.virtualenvs/localpy/bin/python -c "import SedmlToRr"
+RUN         mkdir /home/user/combine2python
+ADD         ./combine2python /home/user/combine2python
+ENV         PYTHONDIR /home/user/.ipython
+ENV         MPLCONFIGDIR /home/user/ipynotebooks
+ENV         HOME /home/user
--- combine2python/combine2python.py
+++ combine2python/combine2python.py
@@ -1,19 +1,25 @@
 import click
 import ipythonify
+import os
 
 
 @click.command()
-@click.option('--input',
+@click.option('--input-str',
               help='Encoded combine archive string to decode.')
 @click.option('--output-path', default='/home/user/workspaces',
               help='Path where combine archive will be unzipped.')
-@click.option('--folder-name',
+@click.option('--project-name',
               help='Name for unzipped archive.')
 @click.option('--encoding', default='base64',
               help='Encoding of combine archive string.')
-def combine2python(inputstr, dirpth, fname, encoding):
-    """Simple program that greets NAME for a total of COUNT times."""
-    ipythonify.str2py(inputstr, dirpth, fname, encoding)
+def combine2python(input_str, output_path, project_name, encoding):
+    """Extracts encoded string containing combine archive
+    and generates Python scripts."""
+    ipythonify.str2py(input_str, output_path, project_name, encoding)
+    notebook = ipythonify.jsonify(output_path, project_name)
+    dstloc = os.path.join(os.path.join(output_path, project_name), project_name + '.ipynb')
+    with open(dstloc, "w") as notebook_file:
+        notebook_file.write(notebook)
 
 if __name__ == '__main__':
     combine2python()
